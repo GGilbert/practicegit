7a6dcc69e248a93f4b8b79e46322cbb9b05724ae
ggilbert <ggilbert@orchestrationservices.com>
2021-11-01 10:34:07 -0400

    adding unordered list <ul></ul> tags

--- resources/views/practice/landing.blade.php
+++ resources/views/practice/landing.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-
+    <title>practice.landing</title>
     </head>
     <body>
+    <ul>
     <li>This is a statement</li>
     <li>This is a statement</li>
     <li>This is a statement</li>
-
     <li>This is a statement</li>
-
+    <li>This is a statement added</li>
+    </ul>
 
     </body>
 </html>
